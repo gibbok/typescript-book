ed986349448e46e5f4b29d684fbb0f9f42aa5c7c
GibboK <gibbok.coding@gmail.com>
2023-06-20 14:42:26 +0200

    save

--- compile.ts
+++ compile.ts
@@ -1,138 +1,50 @@
-import * as fs from 'fs';
 import * as ts from 'typescript';
 
-function extractCodeSnippets(markdown: string): string[] {
-    const codeSnippets: string[] = [];
-    const regex = /```typescript([\s\S]*?)```/g;
-    let match: RegExpExecArray | null;
-
-    while ((match = regex.exec(markdown)) !== null) {
-        const codeSnippet = match[1].trim();
-        codeSnippets.push(codeSnippet);
-    }
-
-    return codeSnippets;
-}
-
-function compileTypeScriptCode(code: string,): ts.Diagnostic[] {
+function checkCodeSnippet(code: string): void {
     const compilerOptions: ts.CompilerOptions = {
-        target: ts.ScriptTarget.ESNext,
-        module: ts.ModuleKind.CommonJS,
+        target: ts.ScriptTarget.ES2017,
+        module: ts.ModuleKind.CommonJS
     };
 
-    const compilerHost: ts.CompilerHost = {
-        getSourceFile: () => ts.createSourceFile('temp.ts', code, ts.ScriptTarget.ESNext),
-        getDefaultLibFileName: () => 'lib.d.ts',
-        writeFile: () => { },
-        getCurrentDirectory: ts.sys.getCurrentDirectory,
-        getDirectories: ts.sys.getDirectories,
-        getCanonicalFileName: ts.sys.useCaseSensitiveFileNames ? (fileName: string) => fileName : (fileName: string) => fileName.toLowerCase(),
-        getNewLine: () => ts.sys.newLine,
+    const host: ts.CompilerHost = {
         fileExists: ts.sys.fileExists,
         readFile: ts.sys.readFile,
-        useCaseSensitiveFileNames: () => false
-    };
-
-    const program = ts.createProgram(['temp.ts'], compilerOptions, compilerHost);
-    const sourceFile = program.getSourceFile('temp.ts');
-    if (sourceFile) {
-        const diagnostics = ts.getPreEmitDiagnostics(program, sourceFile);
-        //@ts-ignore
-        return diagnostics;
-    }
-
-    return [];
-}
-
-function saveErrorsToFile(errors: ts.Diagnostic[], outputFilePath: string): void {
-    const formattedErrors = ts.formatDiagnosticsWithColorAndContext(errors, {
-        getCanonicalFileName: (fileName: string) => fileName,
-        getCurrentDirectory: ts.sys.getCurrentDirectory,
-        getNewLine: () => ts.sys.newLine,
-    });
-
-    fs.writeFileSync(outputFilePath, formattedErrors);
-}
-
-function processMarkdownFile(inputFilePath: string, outputFilePath: string): void {
-    try {
-        const markdown = fs.readFileSync(inputFilePath, 'utf-8');
-        const codeSnippets = extractCodeSnippets(markdown);
-        const errors: ts.Diagnostic[] = [];
-
-        for (const code of codeSnippets) {
-            const codeErrors = compileTypeScriptCode(code);
-            errors.push(...codeErrors);
-        }
-
-        if (errors.length > 0) {
-            saveErrorsToFile(errors, outputFilePath);
-            console.log(`Compilation completed with errors. Error details saved to ${outputFilePath}`);
-        } else {
-            console.log(`Compilation completed successfully.`);
-        }
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
-}
-
-// Usage example
-const inputFilePath = 'test.md';
-const outputFilePath = 'errors.txt';
-processMarkdownFile(inputFilePath, outputFilePath);
-
-/*
-import * as ts from 'typescript';
-
-function compileTypeScriptCode(code: string): ts.Diagnostic[] {
-    const compilerOptions: ts.CompilerOptions = {
-        target: ts.ScriptTarget.ESNext,
-        module: ts.ModuleKind.CommonJS,
-    };
-
-    const compilerHost: ts.CompilerHost = {
-        getSourceFile: () => ts.createSourceFile('temp.ts', code, ts.ScriptTarget.ESNext),
+        getSourceFile: (fileName) => {
+            if (fileName === 'snippet.ts') {
+                return ts.createSourceFile(fileName, code, ts.ScriptTarget.ES2017, true);
+            }
+            return undefined;
+        },
         getDefaultLibFileName: () => 'lib.d.ts',
         writeFile: () => { },
-        getCurrentDirectory: ts.sys.getCurrentDirectory,
-        getDirectories: ts.sys.getDirectories,
-        getCanonicalFileName: ts.sys.useCaseSensitiveFileNames ? (fileName: string) => fileName : (fileName: string) => fileName.toLowerCase(),
+        getCurrentDirectory: () => '',
+        getCanonicalFileName: (fileName) => fileName,
         getNewLine: () => ts.sys.newLine,
-        fileExists: ts.sys.fileExists,
-        readFile: ts.sys.readFile,
-        useCaseSensitiveFileNames: () => false
+        useCaseSensitiveFileNames: () => ts.sys.useCaseSensitiveFileNames,
     };
 
-    const program = ts.createProgram(['temp.ts'], compilerOptions, compilerHost);
-    const sourceFile = program.getSourceFile('temp.ts');
-    if (sourceFile) {
-        const diagnostics = ts.getPreEmitDiagnostics(program, sourceFile);
-        //@ts-ignore
-        return diagnostics;
-    }
+    const program = ts.createProgram(['snippet.ts'], compilerOptions, host);
+    const diagnostics = ts.getPreEmitDiagnostics(program);
 
-    return [];
+    if (diagnostics.length === 0) {
+        console.log('Code snippet is valid.');
+    } else {
+        console.log('Code snippet has errors:');
+        diagnostics.forEach((diagnostic) => {
+            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+            if (diagnostic.file) {
+                const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
+                console.log(`  Error at ${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+            } else {
+                console.log(`  Error: ${message}`);
+            }
+        });
+    }
 }
 
-// Usage example
-const code = `
-  // TypeScript code to compile
-  function greet(name: string) {
-    console.log('Hello, ' + name);
-  }
-
-  greet(123); // Intentional type error
+const codeSnippet = `
+  const x: string = 'Hello, world!';
+  console.log(x);
 `;
 
-const errors = compileTypeScriptCode(code);
-if (errors.length > 0) {
-    const formattedErrors = ts.formatDiagnosticsWithColorAndContext(errors, {
-        getCanonicalFileName: (fileName: string) => fileName,
-        getCurrentDirectory: ts.sys.getCurrentDirectory,
-        getNewLine: () => ts.sys.newLine,
-    });
-    console.error(formattedErrors);
-} else {
-    console.log('Compilation completed successfully.');
-}
-*/
+checkCodeSnippet(codeSnippet);
---
+++ test.ts
@@ -0,0 +1,10 @@
+type Test = {
+    a: string;
+}
+
+const test: Test = {
+    a: 1234
+}
+
+const hello = 'hello'
+console.log(hello)
