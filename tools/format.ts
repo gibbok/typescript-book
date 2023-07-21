6648f1eaf552fe3eda674426910932467a0300c2
GibboK <gibbok.coding@gmail.com>
2023-07-21 13:04:12 +0200

    add logs

--- tools/format.ts
+++ tools/format.ts
@@ -1,10 +1,10 @@
 import * as prettier from 'prettier';
 import * as fs from 'fs';
-import { languages } from './i18n';
+import { Language, languages } from './i18n';
 import { PrettierOptions, getPrettierOptions, makeFilePath } from './utils';
 import { CODE_BLOCK_TS_REGEX } from './config';
 
-const formatCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierOptions): Promise<void> => {
+const formatCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierOptions, language: Language): Promise<void> => {
     const markdown = await fs.promises.readFile(filePath, 'utf-8');
 
     let formattedMarkdown = markdown;
@@ -20,13 +20,13 @@ const formatCodeBlocksInMarkdownFile = async (filePath: string, options: Prettie
     }
 
     await fs.promises.writeFile(filePath, formattedMarkdown, 'utf-8');
-    console.log(`Formatted code blocks have been updated in the file: ${filePath}`);
+    console.log(`${language} Formatted code blocks have been updated in the file: ${filePath}`);
 }
 
 const main = async () => {
     const options = await getPrettierOptions()
-    for (const item of languages) {
-        formatCodeBlocksInMarkdownFile(makeFilePath(item), options);
+    for (const language of languages) {
+        formatCodeBlocksInMarkdownFile(makeFilePath(language), options, language);
     }
 }
 
--- tools/lint.ts
+++ tools/lint.ts
@@ -1,10 +1,10 @@
 import * as prettier from 'prettier';
 import * as fs from 'fs';
-import { languages } from './i18n';
+import { Language, languages } from './i18n';
 import { PrettierOptions, getPrettierOptions, makeFilePath } from './utils';
 import { CODE_BLOCK_TS_REGEX } from './config';
 
-const lintCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierOptions): Promise<void> => {
+const lintCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierOptions, language: Language): Promise<void> => {
     const markdown = await fs.promises.readFile(filePath, 'utf-8');
 
     let reportSnippetsNotLinted = '';
@@ -21,9 +21,9 @@ const lintCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierO
         }
     }
     if (reportSnippetsNotLinted.length === 0) {
-        console.log("All snippets are linted!")
+        console.log(`${language} All snippets are linted!`)
     } else {
-        console.error('Not all snippets are linted! Please use this command to automatically format the snippets: npm run format\n')
+        console.error(`${language} Not all snippets are linted! Please use this command to automatically format the snippets: npm run format\n`)
         console.error(reportSnippetsNotLinted)
         process.exit(1)
     }
@@ -31,8 +31,8 @@ const lintCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierO
 
 const main = async () => {
     const options = await getPrettierOptions()
-    for (const item of languages) {
-        lintCodeBlocksInMarkdownFile(makeFilePath(item), options);
+    for (const language of languages) {
+        lintCodeBlocksInMarkdownFile(makeFilePath(language), options, language);
     }
 }
 
