ab58511630bcefef58fa5b6daaa89f4d0cdf9daf
Simone Poggiali <17195702+gibbok@users.noreply.github.com>
2023-07-21 13:08:21 +0200

    Lint check (#42)
    Lint with Prettier TS code snippets in MD files.

--- tools/format.ts
+++ tools/format.ts
@@ -1,30 +1,36 @@
 import * as prettier from 'prettier';
 import * as fs from 'fs';
 import { Language, languages } from './i18n';
-import { makeFilePath } from './utils';
+import { PrettierOptions, getPrettierOptions, makeFilePath } from './utils';
+import { CODE_BLOCK_TS_REGEX } from './config';
 
-const PRETTIER_CONFIG_FILE_PATH = './.prettierrc'
-
-const formatCodeBlocksInMarkdownFile = async (filePath: string): Promise<void> => {
+const formatCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierOptions, language: Language): Promise<void> => {
     const markdown = await fs.promises.readFile(filePath, 'utf-8');
-    const codeBlockRegex = /```typescript([\s\S]*?)```/g;
 
     let formattedMarkdown = markdown;
     let match;
-    while ((match = codeBlockRegex.exec(markdown)) !== null) {
+    while ((match = CODE_BLOCK_TS_REGEX.exec(markdown)) !== null) {
         const codeBlock = match[0];
         const code = match[1].trim();
         const formattedCode = prettier.format(code, {
             parser: 'typescript',
-            ...(await prettier.resolveConfig(PRETTIER_CONFIG_FILE_PATH)),
+            ...options,
         });
         formattedMarkdown = formattedMarkdown.replace(codeBlock, `\`\`\`typescript\n${formattedCode}\`\`\``);
     }
 
     await fs.promises.writeFile(filePath, formattedMarkdown, 'utf-8');
-    console.log(`Formatted code blocks have been updated in the file: ${filePath}`);
+    console.log(`${language} Formatted code blocks have been updated in the file: ${filePath}`);
 }
 
-for (const item of languages) {
-    formatCodeBlocksInMarkdownFile(makeFilePath(item));
+const main = async () => {
+    const options = await getPrettierOptions()
+
+    for (const language of languages) {
+        formatCodeBlocksInMarkdownFile(makeFilePath(language), options, language);
+    }
 }
+
+main()
+
+
---
+++ tools/lint.ts
@@ -0,0 +1,42 @@
+import * as prettier from 'prettier';
+import * as fs from 'fs';
+import { Language, languages } from './i18n';
+import { PrettierOptions, getPrettierOptions, makeFilePath } from './utils';
+import { CODE_BLOCK_TS_REGEX } from './config';
+
+const lintCodeBlocksInMarkdownFile = async (filePath: string, options: PrettierOptions, language: Language): Promise<void> => {
+    const markdown = await fs.promises.readFile(filePath, 'utf-8');
+
+    let reportSnippetsNotLinted = '';
+    let match;
+    while ((match = CODE_BLOCK_TS_REGEX.exec(markdown)) !== null) {
+        const code = match[1].trim() + '\n'
+        const isCodeLinted = prettier.check(code, {
+            parser: 'typescript',
+            ...options,
+        });
+        if (isCodeLinted === false) {
+            reportSnippetsNotLinted += code
+            reportSnippetsNotLinted += '\n'
+        }
+    }
+    if (reportSnippetsNotLinted.length === 0) {
+        console.log(`${language} All snippets are linted!`)
+    } else {
+        console.error(`${language} Not all snippets are linted! Please use this command to automatically format the snippets: npm run format\n`)
+        console.error(reportSnippetsNotLinted)
+        process.exit(1)
+    }
+}
+
+const main = async () => {
+    const options = await getPrettierOptions()
+
+    for (const language of languages) {
+        lintCodeBlocksInMarkdownFile(makeFilePath(language), options, language);
+    }
+}
+
+main()
+
+
