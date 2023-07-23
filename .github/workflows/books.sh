4969d9939b971c07667148671cf6bdc72a04f19a
GibboK <gibbok.coding@gmail.com>
2023-07-23 09:36:25 +0200

    save

--- .github/workflows/books.sh
+++ .github/workflows/books.sh
@@ -5,4 +5,6 @@ mkdir downloadsXXX
 pandoc -o downloadsXXX/typescript-book.epub --metadata title="The Concise TypeScript Book" --metadata author="Simone Poggiali"  -s README.md
 pandoc -o downloadsXXX/typescript-book-zh_CN.epub --metadata title="# 简洁的TypeScript之书" --metadata author="Simone Poggiali"  -s README-zh_CN.md
 cd downloadsXXX
+git add -A
+git commit -m 'automatically added books'
 ls
