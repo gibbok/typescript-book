3aaaffbd94897eb60c106fd32cc23c06d23562bf
GibboK <gibbok.coding@gmail.com>
2023-07-23 09:34:51 +0200

    save

--- .github/workflows/books.sh
+++ .github/workflows/books.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Making ebooks ..."
 sudo apt-get install pandoc
+mkdir downloadsXXX
 pandoc -o downloadsXXX/typescript-book.epub --metadata title="The Concise TypeScript Book" --metadata author="Simone Poggiali"  -s README.md
 pandoc -o downloadsXXX/typescript-book-zh_CN.epub --metadata title="# 简洁的TypeScript之书" --metadata author="Simone Poggiali"  -s README-zh_CN.md
 cd downloadsXXX
