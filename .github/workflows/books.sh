b82225fb803db1c600e4aad2e47196f127053ff8
GibboK <gibbok.coding@gmail.com>
2023-07-23 09:43:49 +0200

    save

--- .github/workflows/books.sh
+++ .github/workflows/books.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 echo "Making ebooks ..."
 sudo apt-get install pandoc
-mkdir downloadsXXX
-pandoc -o downloadsXXX/typescript-book.epub --metadata title="The Concise TypeScript Book" --metadata author="Simone Poggiali"  -s README.md
-pandoc -o downloadsXXX/typescript-book-zh_CN.epub --metadata title="# 简洁的TypeScript之书" --metadata author="Simone Poggiali"  -s README-zh_CN.md
-cd downloadsXXX
+pandoc -o downloads/typescript-book.epub --metadata title="The Concise TypeScript Book" --metadata author="Simone Poggiali"  -s README.md
+pandoc -o downloads/typescript-book-zh_CN.epub --metadata title="# 简洁的TypeScript之书" --metadata author="Simone Poggiali"  -s README-zh_CN.md
 git config user.name "GitHub Actions Bot"
 git config user.email "<>"
 git add -A
-git commit -m 'automatically added books'
+git commit -m 'Automatically add books'
 git push
-ls
