96501a200b12fcc4003ee2024d596e257351fa38
GibboK <gibbok.coding@gmail.com>
2023-07-23 09:40:47 +0200

    save

--- .github/workflows/books.sh
+++ .github/workflows/books.sh
@@ -5,6 +5,9 @@ mkdir downloadsXXX
 pandoc -o downloadsXXX/typescript-book.epub --metadata title="The Concise TypeScript Book" --metadata author="Simone Poggiali"  -s README.md
 pandoc -o downloadsXXX/typescript-book-zh_CN.epub --metadata title="# 简洁的TypeScript之书" --metadata author="Simone Poggiali"  -s README-zh_CN.md
 cd downloadsXXX
+git config user.name "GitHub Actions Bot"
+git config user.email "<>"
 git add -A
 git commit -m 'automatically added books'
+git push
 ls
