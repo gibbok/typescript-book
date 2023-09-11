df9de549811698931fede5fd02b9d70be9f1d83a
Simone Poggiali <gibbok.coding@gmail.com>
2023-09-11 11:10:07 +0200

    save

--- README.md
+++ README.md
@@ -4866,3 +4866,30 @@ Resources are guaranteed to be disposed, even if subsequent code or exceptions o
 ### await using declaration
 
 An "await using" is like "using" but for asynchronously disposable resources. These resources may involve asynchronous operations, so their disposal should be awaited.
+
+```typescrpt
+//@ts-ignore
+Symbol.asyncDispose ??= Symbol("Symbol.asyncDispose");
+
+class DatabaseConnection implements AsyncDisposable {
+    // A method that is called when the object is disposed asynchronously
+    [Symbol.asyncDispose]() {
+        // Close the connection and return a promise
+        return this.close();
+    }
+
+    async close() {
+        console.log("Closing the connection...");
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        console.log("Connection closed.");
+    }
+}
+
+async function doWork() {
+    // Create a new connection and dispose it asynchronously when it goes out of scope
+    await using connection = new DatabaseConnection();
+    console.log("Doing some work...");
+}
+
+doWork();
+```
--- tools/x.ts
+++ tools/x.ts
@@ -1,32 +1,26 @@
 //@ts-ignore
 Symbol.asyncDispose ??= Symbol("Symbol.asyncDispose");
 
+class DatabaseConnection implements AsyncDisposable {
+    // A method that is called when the object is disposed asynchronously
+    [Symbol.asyncDispose]() {
+        // Close the connection and return a promise
+        return this.close();
+    }
+
+    async close() {
+        console.log("Closing the connection...");
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        console.log("Connection closed.");
+    }
+}
+
+
 async function doWork() {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    // Create a new connection and dispose it asynchronously when it goes out of scope
+    await using connection = new DatabaseConnection();
+    console.log("Doing some work...");
 }
 
-const log = (id: string): AsyncDisposable => {
-    return {
-        async [Symbol.asyncDispose]() {
-            console.log(`disposing (async) ${id}`);
-            await doWork();
-        },
-    }
-}
+doWork();
 
-async function func() {
-    await using a = log("a");
-    await using b = log("b");
-    {
-        await using c = log("c");
-        await using d = log("d");
-    }
-    await using e = log("e");
-    return;
-
-    // Unreachable.
-    // Never created, never disposed.
-    await using f = log("f");
-}
-
-func();
