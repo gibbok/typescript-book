c4966cb42684c548ad426daecc9dad94b0cb9b24
Simone Poggiali <17195702+gibbok@users.noreply.github.com>
2024-01-18 07:47:15 +0100

    Deploy to GitHub Pages setup (#95)

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gibbok.github.io/typescript-book',
+    site: 'https://gibbok.github.io',
     integrations: [
         starlight({
             title: 'TypeScript Book',
