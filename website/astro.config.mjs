b96d7df486f010d2f80b1533a02b2d35ccf87a8d
Simone Poggiali <gibbok.coding@gmail.com>
2024-01-18 07:55:30 +0100

    fix links

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gibbok.github.io',
+    base: '/typescript-book',
     integrations: [
         starlight({
             title: 'TypeScript Book',
