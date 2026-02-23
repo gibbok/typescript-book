fc34ac441a16ac63eb51288486f720ba0ad06de7
Radosvet Petrov <93radosvet@gmail.com>
2026-02-23 22:40:05 +0000

    feat(lang-BG): added bulgarian setup

--- tools/i18n.ts
+++ tools/i18n.ts
@@ -3,7 +3,8 @@ export enum Language {
     zh_CN = 'zh_CN',
     it_IT = 'it_IT',
     pt_BR = 'pt_BR',
+    bg_BG = 'bg_BG',
 }
-export type Languages = [Language.en_EN, Language.zh_CN, Language.it_IT, Language.pt_BR]
+export type Languages = [Language.en_EN, Language.zh_CN, Language.it_IT, Language.pt_BR, Language.bg_BG]
 
-export const languages: Languages = [Language.en_EN, Language.zh_CN, Language.it_IT, Language.pt_BR]
+export const languages: Languages = [Language.en_EN, Language.zh_CN, Language.it_IT, Language.pt_BR, Language.bg_BG]
